d1f51d1c4d0c115d596b7584e8bf5646c3810525
bojan987 <bojanmaj@hotmail.com>
2020-09-25 10:52:10 +0200

    Updates

---
+++ precache-manifest.abe2037319d5cbeb861b46ef4d1b13d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927a73f422f67e58d6c5b2b6ab84d167",
+    "url": "/Projekat/index.html"
+  },
+  {
+    "revision": "9a1c2ffe1a267ce218ff",
+    "url": "/Projekat/static/css/2.802de1e8.chunk.css"
+  },
+  {
+    "revision": "f976c0621d8f9372d721",
+    "url": "/Projekat/static/css/main.3fd185e5.chunk.css"
+  },
+  {
+    "revision": "9a1c2ffe1a267ce218ff",
+    "url": "/Projekat/static/js/2.1410f374.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/Projekat/static/js/2.1410f374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f976c0621d8f9372d721",
+    "url": "/Projekat/static/js/main.b73a4782.chunk.js"
+  },
+  {
+    "revision": "67ab63a192e71730942a",
+    "url": "/Projekat/static/js/runtime-main.539e4e33.js"
+  },
+  {
+    "revision": "66ff4160d930e0306b7248538b3e4831",
+    "url": "/Projekat/static/media/HeroHunt.66ff4160.jpg"
+  },
+  {
+    "revision": "5c7f35638573815b27415e36fc30e87a",
+    "url": "/Projekat/static/media/jugger.5c7f3563.png"
+  },
+  {
+    "revision": "68ecbd3f14dc0ac620c8c0c7dd3c5dac",
+    "url": "/Projekat/static/media/loginImage.68ecbd3f.jpg"
+  },
+  {
+    "revision": "d11b50a8899920cd606dbb17bc0cd60a",
+    "url": "/Projekat/static/media/morph.d11b50a8.jpg"
+  },
+  {
+    "revision": "23dddef471843421afb29498e20c2f42",
+    "url": "/Projekat/static/media/phoenix.23dddef4.png"
+  }
+]);
